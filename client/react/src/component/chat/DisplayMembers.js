b6c5ba28e4cb4d694ebf96a826e18045d4df8117
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2022-03-26 21:15:03 -0400

    key issue fixes

--- client/react/src/component/chat/DisplayMembers.js
+++ client/react/src/component/chat/DisplayMembers.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function DisplayMembers(props) {
     var members = props.members.map(function(member,i){
-        return <p>{member.username}</p>
+        return <p key={uuidv4()}>{member.username}</p>
     })
     return (
         <div className="pl-4 pt-4 w-full h-full flex-col space-y-2">
